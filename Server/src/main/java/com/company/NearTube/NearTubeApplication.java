1fd1bd9f20591a8a1da8ba342cf4f943277a5b83
hoony0321 <eogns0321@gmail.com>
2023-04-19 20:59:25 +0900

    init spring project

---
+++ Server/src/main/java/com/company/NearTube/NearTubeApplication.java
@@ -0,0 +1,13 @@
+package com.company.NearTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearTubeApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/company/NearTube/NearTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.NearTube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
